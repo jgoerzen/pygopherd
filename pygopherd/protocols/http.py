b125e3fc88e28c7d9a5af5514fbf9e8f0fe3c5ab
jgoerzen <jgoerzen>
2002-03-28 01:19:00 +0100

    /pygopherd/head: changeset 59
    Added error handling

--- pygopherd/protocols/http.py
+++ pygopherd/protocols/http.py
@@ -7,8 +7,6 @@ import cgi, GopherExceptions
 class HTTPProtocol(protocols.base.BaseGopherProtocol):
     def canhandlerequest(self):
         self.requestparts = map(lambda arg: arg.strip(), self.request.split(" "))
-        print 'len', len(self.requestparts)
-        print self.requestparts[0]
         return len(self.requestparts) == 3 and \
                (self.requestparts[0] == 'GET' or self.requestparts[0] == 'HEAD') and \
                self.requestparts[2][0:5] == 'HTTP/'
@@ -23,7 +21,6 @@ class HTTPProtocol(protocols.base.BaseGopherProtocol):
         self.selector = urllib.unquote(self.requestparts[1])
 
         try:
-            print 'HTTP: getting handler for', self.selector
             handler = self.gethandler()
             self.entry = handler.getentry()
             handler.prepare()
@@ -85,8 +82,8 @@ class HTTPProtocol(protocols.base.BaseGopherProtocol):
         return retstr
     
     def renderdirstart(self, entry):
-        retstr ='<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">\n'
-        retstr += "<HTML><HEAD><TITLE>Gopher"
+        retstr ='<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">'
+        retstr += "\n<HTML><HEAD><TITLE>Gopher"
         if self.entry.getname():
             retstr += ": " + cgi.escape(self.entry.getname())
         retstr += "</TITLE></HEAD><BODY><H1>Gopher"
@@ -97,3 +94,13 @@ class HTTPProtocol(protocols.base.BaseGopherProtocol):
 
     def renderdirend(self, entry):
         return '</TABLE></BODY></HTML>'
+
+    def filenotfound(self, msg):
+        self.wfile.write("HTTP/1.0 404 Not Found\n")
+        self.wfile.write("Content-Type: text/html\n\n")
+        self.wfile.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">')
+        self.wfile.write("""\n<HTML><HEAD><TITLE>Selector Not Found</TITLE>
+        <H1>Selector Not Found</H1>
+        <TT>""")
+        self.wfile.write(cgi.escape(msg))
+        self.wfile.write("</TT><HR>Pygopherd</BODY></HTML>\n")
