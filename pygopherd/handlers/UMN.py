db2fbc3805db43622dfdcd2e03d02a5e0ba60955
jgoerzen <jgoerzen>
2002-04-06 04:12:49 +0100

    /pygopherd/head: changeset 78
    Added some comments and moved miscellaneous functions into the
    UMNDirHandler.

--- pygopherd/handlers/UMN.py
+++ pygopherd/handlers/UMN.py
@@ -5,37 +5,6 @@ import handlers, handlers.base
 from gopherentry import GopherEntry
 from handlers.dir import DirHandler
 
-def sgn(a):
-    """Returns -1 if less than 0, 1 if greater than 0, and 0 if
-    equal to zero."""
-    if a == 0:
-        return 0
-    if a < 0:
-        return -1
-    return 1
-
-def entrycmp(entry1, entry2):
-    """This function implements an exact replica of UMN behavior
-    GSqsortcmp() behavior."""
-    if entry1.getname() == None:
-        return 1
-    if entry2.getname() == None:
-        return -1
-
-    # Equal numbers or no numbers: sort by title.
-    if entry1.getnum() == entry2.getnum():
-        return cmp(entry1.getname(), entry2.getname())
-
-    # Same signs: use plain numeric comparison.
-    if (sgn(entry1.getnum()) == sgn(entry2.getnum())):
-        return cmp(entry1.getnum(), entry2.getnum())
-
-    # Different signs: other comparison.
-    if entry1.getnum() > entry2.getnum():
-        return -1
-    else:
-        return 1
-
 class LinkEntry(GopherEntry):
     def __init__(self, selector, config):
         GopherEntry.__init__(self, selector, config)
@@ -50,10 +19,17 @@ class UMNDirHandler(DirHandler):
 
     def prepare(self):
         """Override parent to do a few more things and override sort order."""
+
+        # Initialize.
         self.linkentries = []
+
+        # Let the parent do the directory walking for us.  Will call
+        # prep_initfiles_canaddfile and prep_entriesappend.
         DirHandler.prepare(self)
+
+        # Merge and sort.
         self.MergeLinkFiles()
-        self.fileentries.sort(entrycmp)
+        self.fileentries.sort(self.entrycmp)
         
     def prep_initfiles_canaddfile(self, ignorepatt, pattern, file):
         """Override the parent to process dotfiles and keep them out
@@ -65,11 +41,32 @@ class UMNDirHandler(DirHandler):
             if file[0] == '.' and not os.path.isdir(self.fsbase + '/' + file):
                 self.linkentries.extend(self.processLinkFile(self.fsbase + '/' + file))
                 return 0
-            return 1
+            return 1                    # Not a dot file -- return true
         else:
-            return 0
+            return 0                    # Parent returned 0, do the same.
+
+    def prep_entriesappend(self, file, fileentry):
+        """Overridden to process .cap files.  This is called by the
+        parent's prepare to append an entry to the list.  Here, we check
+        to see if there's a .cap file right before adding it."""
+        capfilename = self.fsbase + '/.cap/' + file
+        if os.path.isfile(capfilename):
+            capinfo = self.processLinkFile(capfilename,
+                                           fileentry.getselector())
+            if len(capinfo) >= 1:       # We handle one and only one entry.
+                if capinfo[0].gettype() == 'X':
+                    return              # Type X -- don't append.
+                else:
+                    self.mergeentries(fileentry, capinfo[0])
+        DirHandler.prep_entriesappend(self, file, fileentry)
 
     def MergeLinkFiles(self):
+        """Called to merge the files from .Links and .names into the
+        objects obtained by walking the directory.  According to UMN code,
+        we ONLY merge if the Path starts with ./ or ~/ in the file.  This
+        is set in the getneedsmerge() attribute.  If that attribute is
+        not set, don't even bother with it -- just add."""
+        
         for linkentry in self.linkentries:
             if not linkentry.getneedsmerge():
                 self.fileentries.append(linkentry)
@@ -93,25 +90,15 @@ class UMNDirHandler(DirHandler):
                 self.fileentries.append(linkentry)
 
     def mergeentries(self, old, new):
+        """Takes the set fields from new and modifies old to have their
+        value."""
         for field in ['selector', 'type', 'name', 'host', 'port']:
             if getattr(new, field):
                 setattr(old, field, getattr(new, field))
 
-
-    def prep_entriesappend(self, file, fileentry):
-        """Overridden to process .cap files."""
-        capfilename = self.fsbase + '/.cap/' + file
-        if os.path.isfile(capfilename):
-            capinfo = self.processLinkFile(capfilename,
-                                           fileentry.getselector())
-            if len(capinfo) >= 1:       # We handle one and only one entry.
-                if capinfo[0].gettype() == 'X':
-                    return
-                else:
-                    self.mergeentries(fileentry, capinfo[0])
-        DirHandler.prep_entriesappend(self, file, fileentry)
-
     def processLinkFile(self, filename, capfilepath = None):
+        """Processes a link file.  If capfilepath is set, it should
+        be the equivolent of the Path= in a .names file."""
         linkentries = []
         fd = open(filename, "rt")
         while 1:
@@ -192,3 +179,35 @@ class UMNDirHandler(DirHandler):
         if done['path']:
             return (nextstep, entry)
         return (nextstep, None)
+
+    def sgn(self, a):
+        """Returns -1 if less than 0, 1 if greater than 0, and 0 if
+        equal to zero."""
+        if a == 0:
+            return 0
+        if a < 0:
+            return -1
+        return 1
+
+    def entrycmp(self, entry1, entry2):
+        """This function implements an exact replica of UMN behavior
+        GSqsortcmp() behavior."""
+        if entry1.getname() == None:
+            return 1
+        if entry2.getname() == None:
+            return -1
+
+        # Equal numbers or no numbers: sort by title.
+        if entry1.getnum() == entry2.getnum():
+            return cmp(entry1.getname(), entry2.getname())
+
+        # Same signs: use plain numeric comparison.
+        if (self.sgn(entry1.getnum()) == self.sgn(entry2.getnum())):
+            return cmp(entry1.getnum(), entry2.getnum())
+
+        # Different signs: other comparison.
+        if entry1.getnum() > entry2.getnum():
+            return -1
+        else:
+            return 1
+
