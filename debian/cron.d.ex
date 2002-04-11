0b0b921a4a8728e9189814bf14a19ea7b1a862f4
jgoerzen <jgoerzen>
2002-04-11 01:10:58 +0100

    /pygopherd/head: changeset 103
    Imported debian/ directory.

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the pygopherd package
+#
+0 4	* * *	root	pygopherd_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(pygopherd):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="pygopherd" command="/usr/bin/pygopherd"
