234fa58d0250b65e4b23b5151d41ae4175d7fcdd
jgoerzen <jgoerzen>
2003-12-18 00:29:06 +0100

    /pygopherd/head: changeset 306
    Fixed indentation

--- pygopherd/handlers/tal.py
+++ pygopherd/handlers/tal.py
@@ -17,7 +17,7 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 try:
-from simpletal import simpleTAL, simpleTALES
+    from simpletal import simpleTAL, simpleTALES
     talavailable = 1
 except:
     talavailable = 0
