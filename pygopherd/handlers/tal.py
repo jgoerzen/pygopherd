8a9f7bebd9855abf81ec0f54a4fee7635d0678ad
jgoerzen <jgoerzen>
2003-12-18 00:26:43 +0100

    /pygopherd/head: changeset 305
    Don't crash if tal is unavailable.

--- pygopherd/handlers/tal.py
+++ pygopherd/handlers/tal.py
@@ -16,11 +16,11 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-#try:
+try:
 from simpletal import simpleTAL, simpleTALES
-#    talavailable = 1
-#except:
-#    talavailable = 0
+    talavailable = 1
+except:
+    talavailable = 0
 
 try:
     import logging
