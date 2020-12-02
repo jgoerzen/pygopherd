41b18063be6aefd94420fbc7004c9b6c5f6dc15e
Michael Lazar <mlazar@doctorondemand.com>
2020-12-01 23:33:33 -0500

    PY3: Finish porting mbox handler to python 3 and add test

--- pygopherd/handlers/mbox.py
+++ pygopherd/handlers/mbox.py
@@ -16,13 +16,13 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
-import os
+import io
 import os.path
 import re
 import typing
+import unittest
 import stat
-from mailbox import Maildir, mbox
+from mailbox import Maildir, Message, mbox
 
 from pygopherd import gopherentry
 from pygopherd.handlers.base import VFS_Real
@@ -49,23 +49,18 @@ class FolderHandler(Virtual):
 
     def prepare(self):
         self.entries = []
-        count = 1
-        while 1:
-            message = next(self.mbox)
-            if not message:
-                break
+        for key, message in self.mbox.iteritems():
             handler = MessageHandler(
-                self.genargsselector(self.getargflag() + str(count)),
+                self.genargsselector(self.getargflag() + str(key)),
                 self.searchrequest,
                 self.protocol,
                 self.config,
                 None,
             )
             self.entries.append(handler.getentry(message))
-            count += 1
 
     def isdir(self):
-        return 1
+        return True
 
     def getdirlist(self):
         return self.entries
@@ -73,23 +68,23 @@ class FolderHandler(Virtual):
     def getargflag(self) -> str:
         raise NotImplementedError
 
-    def openmailbox(self):
-        raise NotImplementedError
-
 
 class MessageHandler(Virtual):
+
+    msgnum: int
+    message: Message
+
     def canhandlerequest(self):
         """We put MBOX-MESSAGE in here so we don't have to re-check
         the first line of the mbox file before returning a true or false
         result."""
         if not self.selectorargs:
-            return 0
+            return False
         msgnum = re.search("^" + self.getargflag() + r"(\d+)$", self.selectorargs)
         if not msgnum:
-            return 0
+            return False
         self.msgnum = int(msgnum.group(1))
-        self.message = None
-        return 1
+        return True
 
     def getentry(self, message=None):
         """Set the message if called from, eg, the dir handler.  Saves
@@ -103,7 +98,7 @@ class MessageHandler(Virtual):
             self.entry.setmimetype("text/plain")
             self.entry.setgopherpsupport(0)
 
-            subject = message.getheader("Subject", "<no subject>")
+            subject = message.get("Subject", "<no subject>")
             # Sanitize, esp. for continuations.
             subject = re.sub(r"\s+", " ", subject)
             if subject:
@@ -113,32 +108,19 @@ class MessageHandler(Virtual):
         return self.entry
 
     def getmessage(self):
-        if self.message:
+        if hasattr(self, "message"):
             return self.message
+
         mbox = self.openmailbox()
-        message = None
-        for x in range(self.msgnum):
-            message = next(mbox)
-        self.message = message
+        self.message = mbox.get_message(self.msgnum)
         return self.message
 
     def prepare(self):
         self.canhandlerequest()  # Init the vars
 
     def write(self, wfile):
-        # Print out the headers first.
-        for header in self.getmessage().headers:
-            wfile.write(header)
-
-        # Now the message body.
-        self.rfile = self.getmessage().fp
-        while 1:
-            string = self.rfile.read(4096)
-            if not len(string):
-                break
-            wfile.write(string)
-        self.rfile.close()
-        self.rfile = None
+        message = self.getmessage()
+        wfile.write(message.as_bytes())
 
     def getargflag(self) -> str:
         raise NotImplementedError
@@ -150,37 +132,41 @@ class MessageHandler(Virtual):
 ###########################################################################
 # Unix MBOX support
 ###########################################################################
-
-
 class MBoxFolderHandler(FolderHandler):
     def canhandlerequest(self):
         """Figure out if this is a handleable request."""
+        # Must be a real file
+        if (
+            not isinstance(self.vfs, VFS_Real)
+            or self.selectorargs
+            or not self.statresult
+            or not stat.S_ISREG(self.statresult[stat.ST_MODE])
+        ):
+            return False
 
-        if not isinstance(self.vfs, VFS_Real):
-            return 0
-        if self.selectorargs:
-            return 0
-        if not (self.statresult and stat.S_ISREG(self.statresult[stat.ST_MODE])):
-            return 0
         try:
             with self.vfs.open(self.getselector(), "rb") as fd:
                 startline = fd.readline()
-
-            # From old Python2.7 UnixMailbox
-            fromlinepattern = (
-                rb"From \s*[^\s]+\s+\w\w\w\s+\w\w\w\s+\d?\d\s+"
-                rb"\d?\d:\d\d(:\d\d)?(\s+[^\s]+)?\s+\d\d\d\d\s*"
-                rb"[^\s]*\s*"
-                b"$"
-            )
-
-            return re.match(fromlinepattern, startline)
         except IOError:
-            return 0
+            return False
+
+        # Python 2 had an old "UnixMailbox" class that had more strict
+        # pattern matching on the message "From:" line. This was deprecated
+        # as early as python 2.5 and was dropped completely in python 3.
+        # Since we are using the first line of the file to determine if the
+        # filetype is a mbox or not, it's safer to be stricter with the
+        # pattern matching and port over the old UnixMailbox pattern.
+        fromlinepattern = (
+            rb"From \s*[^\s]+\s+\w\w\w\s+\w\w\w\s+\d?\d\s+"
+            rb"\d?\d:\d\d(:\d\d)?(\s+[^\s]+)?\s+\d\d\d\d\s*"
+            rb"[^\s]*\s*"
+            b"$"
+        )
+        return re.match(fromlinepattern, startline)
 
     def prepare(self):
-        self.mbox = mbox(self.getfspath())
-        FolderHandler.prepare(self)
+        self.mbox = mbox(self.getfspath(), create=False)
+        super().prepare()
 
     def getargflag(self):
         return "/MBOX-MESSAGE/"
@@ -191,9 +177,63 @@ class MBoxMessageHandler(MessageHandler):
         return "/MBOX-MESSAGE/"
 
     def openmailbox(self):
-        with self.vfs.open(self.getselector(), "r") as fp:
-            # TODO: This is dead code, UnixMailbox is not defined
-            return UnixMailbox(fp)
+        return mbox(self.getfspath(), create=False)
+
+
+class TestMBoxFolderHandler(unittest.TestCase):
+    def setUp(self) -> None:
+        from pygopherd import testutil
+
+        self.config = testutil.getconfig()
+        self.vfs = VFS_Real(self.config)
+        self.selector = "/python-dev.mbox"
+        self.protocol = testutil.gettestingprotocol(self.selector, config=self.config)
+        self.stat_result = self.vfs.stat(self.selector)
+
+    def test_mbox_folder_handler(self):
+        handler = MBoxFolderHandler(
+            self.selector, "", self.protocol, self.config, self.stat_result, self.vfs
+        )
+        handler.prepare()
+
+        self.assertTrue(handler.canhandlerequest())
+        self.assertTrue(handler.isdir())
+
+        entry = handler.getentry()
+        self.assertEqual(entry.mimetype, "application/gopher-menu")
+        self.assertEqual(entry.type, "1")
+
+        mbox_entries = handler.getdirlist()
+        self.assertTrue(len(mbox_entries), 6)
+        self.assertEqual(mbox_entries[0].selector, "/python-dev.mbox|/MBOX-MESSAGE/0")
+        self.assertEqual(mbox_entries[0].name, "[Python-Dev] Pickling w/ low overhead")
+
+    def test_mbox_mesage_handler(self):
+        """
+        Load the third message from the mailbox.
+        """
+        handler = MBoxMessageHandler(
+            "/python-dev.mbox|/MBOX-MESSAGE/2",
+            "",
+            self.protocol,
+            self.config,
+            self.stat_result,
+            self.vfs,
+        )
+        handler.prepare()
+
+        self.assertTrue(handler.canhandlerequest())
+        self.assertFalse(handler.isdir())
+
+        entry = handler.getentry()
+        self.assertEqual(entry.mimetype, "text/plain")
+        self.assertEqual(entry.name, "Re: [Python-Dev] Buffer interface in abstract.c?")
+        self.assertEqual(entry.type, "0")
+
+        wfile = io.BytesIO()
+        handler.write(wfile)
+        email_text = wfile.getvalue()
+        assert email_text.startswith(b"From: Greg Stein <gstein@lyra.org>")
 
 
 ###########################################################################
