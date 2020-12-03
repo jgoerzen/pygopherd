4027ecca6642062467a163e03dacfffb0f43fd52
Michael Lazar <mlazar@doctorondemand.com>
2020-12-03 00:31:04 -0500

    PY3: Add test coverage for dir handler

--- pygopherd/handlers/dir.py
+++ pygopherd/handlers/dir.py
@@ -15,20 +15,22 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+import unittest
 import pickle
 import re
 import stat
 import time
-import typing
+import os
 
 from pygopherd import gopherentry, handlers
-from pygopherd.handlers import base
+from pygopherd.handlers.base import VFS_Real, BaseHandler
 
 
-class DirHandler(base.BaseHandler):
+class DirHandler(BaseHandler):
     cachetime: int
     cachefile: str
+    cachename: str
+    fromcache: bool
 
     def canhandlerequest(self) -> bool:
         """We can handle the request if it's for a directory."""
@@ -74,7 +76,7 @@ class DirHandler(base.BaseHandler):
             self.prep_entriesappend(file, handler, fileentry)
 
     def prep_entriesappend(
-        self, file: str, handler: base.BaseHandler, fileentry: gopherentry.GopherEntry
+        self, file: str, handler: BaseHandler, fileentry: gopherentry.GopherEntry
     ):
         """Subclasses can override to do post-processing on the entry while
         we still have the filename around.
@@ -108,24 +110,24 @@ class DirHandler(base.BaseHandler):
         return self.fileentries
 
     def loadcache(self) -> bool:
-
-        self.fromcache = 0
+        self.fromcache = False
         if not hasattr(self, "cachetime"):
             self.cachetime = self.config.getint("handlers.dir.DirHandler", "cachetime")
             self.cachefile = self.config.get("handlers.dir.DirHandler", "cachefile")
-        cachename = self.selector + "/" + self.cachefile
-        if not self.vfs.iswritable(cachename):
+            self.cachename = self.selector + "/" + self.cachefile
+
+        if not self.vfs.iswritable(self.cachename):
             return False
 
         try:
-            statval = self.vfs.stat(cachename)
+            statval = self.vfs.stat(self.cachename)
         except OSError:
             return False
 
         if time.time() - statval[stat.ST_MTIME] < self.cachetime:
-            with self.vfs.open(cachename, "rb") as fp:
+            with self.vfs.open(self.cachename, "rb") as fp:
                 self.fileentries = pickle.load(fp)
-            self.fromcache = 1
+            self.fromcache = True
             return True
         return False
 
@@ -133,10 +135,59 @@ class DirHandler(base.BaseHandler):
         if self.fromcache:
             # Don't resave the cache.
             return
-        if not self.vfs.iswritable(self.selector + "/" + self.cachefile):
+        if not self.vfs.iswritable(self.cachename):
             return
         try:
-            with self.vfs.open(self.selector + "/" + self.cachefile, "wb") as fp:
+            with self.vfs.open(self.cachename, "wb") as fp:
                 pickle.dump(self.fileentries, fp, 1)
         except IOError:
             pass
+
+
+class TestDirHandler(unittest.TestCase):
+    def setUp(self) -> None:
+        from pygopherd import testutil
+
+        self.config = testutil.getconfig()
+        self.vfs = VFS_Real(self.config)
+        self.selector = "/"
+        self.protocol = testutil.gettestingprotocol(self.selector, config=self.config)
+        self.stat_result = self.vfs.stat(self.selector)
+
+        # Make sure there's no directory cache file from a previous test run
+        cachefile = self.config.get("handlers.dir.DirHandler", "cachefile")
+        try:
+            os.remove(self.vfs.getfspath(self.selector) + "/" + cachefile)
+        except OSError:
+            pass
+
+    def test_dir_handler(self):
+        handler = DirHandler(
+            self.selector, "", self.protocol, self.config, self.stat_result, self.vfs
+        )
+
+        self.assertTrue(handler.canhandlerequest())
+        self.assertTrue(handler.isdir())
+
+        handler.prepare()
+        self.assertFalse(handler.fromcache)
+
+        entry = handler.getentry()
+        self.assertEqual(entry.mimetype, "application/gopher-menu")
+        self.assertEqual(entry.type, "1")
+
+        entries = handler.getdirlist()
+        self.assertTrue(entries)
+
+        # Create a second handler to test that it will load from the cached
+        # file that the first handler should have created
+        handler = DirHandler(
+            self.selector, "", self.protocol, self.config, self.stat_result, self.vfs
+        )
+
+        handler.prepare()
+        self.assertTrue(handler.fromcache)
+
+        cached_entries = handler.getdirlist()
+        for a, b in zip(entries, cached_entries):
+            self.assertEqual(a.selector, b.selector)
