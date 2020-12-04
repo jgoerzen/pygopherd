2c9e3392c7c11715c509d4b0b25947f3941f9c98
Michael Lazar <mlazar@doctorondemand.com>
2020-12-03 23:37:13 -0500

    PY3: Fix abstract handler tests after adding test data to directory

--- pygopherd/protocols/rfc1436Test.py
+++ pygopherd/protocols/rfc1436Test.py
@@ -91,14 +91,6 @@ class RFC1436TestCase(unittest.TestCase):
         self.config.set("handlers.ZIP.ZIPHandler", "enabled", "false")
 
     def testhandle_dir_abstracts(self):
-
-        # This test assumes that .zip is in the mimetypes definition but .gz and .tar.gz aren't.
-        # I'm not sure exactly why this is, but I'm guessing it's OS-specific to whatever machine
-        # this unit test was generated on. So this is a hacky fix to get it to pass.
-        fileext.init()
-        fileext.typemap.pop(".gz", None)
-        fileext.typemap.pop(".tgz", None)
-
         proto = GopherProtocol(
             "", self.server, self.handler, self.rfile, self.wfile, self.config
         )
@@ -108,69 +100,34 @@ class RFC1436TestCase(unittest.TestCase):
             self.logfile.getvalue(), "10.77.77.77 [GopherProtocol/UMNDirHandler]: /\n"
         )
         # Try to make this easy on us to fix.
-        actualarr = self.wfile.getvalue().decode().splitlines()
-        expectedarr = [
+        actuallines = self.wfile.getvalue().decode().splitlines()
+        expectedlines = [
             "iThis is the abstract for the testdata directory.\tfake\t(NULL)\t0",
-            "0README\t/README\tHOSTNAME\t64777\t+",
-            "1pygopherd\t/pygopherd\tHOSTNAME\t64777\t+",
-            "9symlinktest\t/symlinktest.zip\tHOSTNAME\t64777\t+",
-            "9testarchive\t/testarchive.tar\tHOSTNAME\t64777\t+",
-            "9testarchive.tar.gz\t/testarchive.tar.gz\tHOSTNAME\t64777\t+",
-            "9testarchive.tgz\t/testarchive.tgz\tHOSTNAME\t64777\t+",
-            "9testdata\t/testdata.zip\tHOSTNAME\t64777\t+",
-            "9testdata2\t/testdata2.zip\tHOSTNAME\t64777\t+",
-            "0testfile\t/testfile.txt\tHOSTNAME\t64777\t+",
-            "0testfile.pyg\t/testfile.pyg\tHOSTNAME\t64777\t+",
-            "9testfile.txt.gz\t/testfile.txt.gz\tHOSTNAME\t64777\t+",
             "iThis is the abstract\tfake\t(NULL)\t0",
             "ifor testfile.txt.gz\tfake\t(NULL)\t0",
-            "9ziptorture\t/ziptorture.zip\tHOSTNAME\t64777\t+",
         ]
-        expectedarr = [
-            re.sub("HOSTNAME", self.server.server_name, x) for x in expectedarr
-        ]
-        self.assertEqual(len(actualarr), len(expectedarr), str(actualarr))
-        for i in range(len(actualarr)):
-            self.assertEqual(actualarr[i], expectedarr[i])
+
+        for line in expectedlines:
+            self.assertIn(line, actuallines)
 
         # Make sure proper line endings are present.
         self.assertEqual(
-            "\r\n".join(actualarr) + "\r\n", self.wfile.getvalue().decode()
+            "\r\n".join(actuallines) + "\r\n", self.wfile.getvalue().decode()
         )
 
     def testhandle_dir_noabstract(self):
-
-        # This test assumes that .zip is in the mimetypes definition but .gz and .tar.gz aren't.
-        # I'm not sure exactly why this is, but I'm guessing it's OS-specific to whatever machine
-        # this unit test was generated on. So this is a hacky fix to get it to pass.
-        fileext.init()
-        fileext.typemap.pop(".gz", None)
-        fileext.typemap.pop(".tgz", None)
-
         self.config.set("pygopherd", "abstract_headers", "off")
         self.config.set("pygopherd", "abstract_entries", "off")
         proto = GopherProtocol(
             "", self.server, self.handler, self.rfile, self.wfile, self.config
         )
         proto.handle()
-        actualarr = self.wfile.getvalue().decode().splitlines()
-        expectedarr = [
-            "0README\t/README\tHOSTNAME\t64777\t+",
-            "1pygopherd\t/pygopherd\tHOSTNAME\t64777\t+",
-            "9symlinktest\t/symlinktest.zip\tHOSTNAME\t64777\t+",
-            "9testarchive\t/testarchive.tar\tHOSTNAME\t64777\t+",
-            "9testarchive.tar.gz\t/testarchive.tar.gz\tHOSTNAME\t64777\t+",
-            "9testarchive.tgz\t/testarchive.tgz\tHOSTNAME\t64777\t+",
-            "9testdata\t/testdata.zip\tHOSTNAME\t64777\t+",
-            "9testdata2\t/testdata2.zip\tHOSTNAME\t64777\t+",
-            "0testfile\t/testfile.txt\tHOSTNAME\t64777\t+",
-            "0testfile.pyg\t/testfile.pyg\tHOSTNAME\t64777\t+",
-            "9testfile.txt.gz\t/testfile.txt.gz\tHOSTNAME\t64777\t+",
-            "9ziptorture\t/ziptorture.zip\tHOSTNAME\t64777\t+",
+        actuallines = self.wfile.getvalue().decode().splitlines()
+        expectedlines = [
+            "iThis is the abstract for the testdata directory.\tfake\t(NULL)\t0",
+            "iThis is the abstract\tfake\t(NULL)\t0",
+            "ifor testfile.txt.gz\tfake\t(NULL)\t0",
         ]
-        expectedarr = [
-            re.sub("HOSTNAME", self.server.server_name, x) for x in expectedarr
-        ]
-        self.assertEqual(len(actualarr), len(expectedarr))
-        for i in range(len(actualarr)):
-            self.assertEqual(actualarr[i], expectedarr[i])
+
+        for line in expectedlines:
+            self.assertNotIn(line, actuallines)
