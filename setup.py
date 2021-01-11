4cdeeabd8bd1b7de595fcf04d4352f2be65ec8ed
Michael Lazar <lazar.michael22@gmail.com>
2021-01-10 23:28:24 -0500

    PACKAGE: Switch from distutils to setuptools, add more package attributes

--- setup.py
+++ setup.py
@@ -19,19 +19,19 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # END OF COPYRIGHT #
+import setuptools
 
-
-from distutils.core import setup
-
-setup(
+setuptools.setup(
     name="pygopherd",
     version="2.0.18",
     description="Multiprotocol Internet Gopher Information Server",
     author="Michael Lazar",
     author_email="lazar.michael22@gmail.com",
+    python_requires=">=3.7",
     url="https://www.github.com/michael-lazar/pygopherd",
     packages=["pygopherd", "pygopherd.handlers", "pygopherd.protocols"],
     scripts=["bin/pygopherd"],
     data_files=[("/etc/pygopherd", ["conf/pygopherd.conf", "conf/mime.types"])],
+    test_suite="pygopherd",
     license="GPLv2",
 )
