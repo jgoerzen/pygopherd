bef3aa3426cb5f4c5a34ad93ce0b0da8c2226b68
Michael Lazar <lazar.michael22@gmail.com>
2021-01-16 13:45:07 -0500

    PY3: Add HTTP protocol test for non-utf8 bytes

--- pygopherd/protocols/http.py
+++ pygopherd/protocols/http.py
@@ -27,7 +27,7 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-from pygopherd import GopherExceptions
+from pygopherd import GopherExceptions, testutil
 from pygopherd.protocols.base import BaseGopherProtocol
 
 
@@ -229,8 +229,6 @@ icons = {
 
 class TestHTTPProtocol(unittest.TestCase):
     def setUp(self):
-        from pygopherd import testutil
-
         self.config = testutil.getconfig()
         self.logfile = testutil.getstringlogger()
         self.rfile = io.BytesIO(b"Accept:text/plain\nHost:localhost.com\n\n")
@@ -257,6 +255,32 @@ class TestHTTPProtocol(unittest.TestCase):
         self.assertIn("HTTP/1.0 200 OK", response)
         self.assertIn("Content-Type: text/html", response)
         self.assertIn('SRC="/PYGOPHERD-HTTPPROTO-ICONS/text.gif"', response)
+
+    @unittest.skipUnless(
+        testutil.supports_non_utf8_filenames(),
+        reason="Filesystem does not support non-utf8 filenames.",
+    )
+    def test_http_hander_non_utf8(self):
+        request = "GET / HTTP/1.1"
+        protocol = HTTPProtocol(
+            request,
+            self.handler.server,
+            self.handler,
+            self.rfile,
+            self.wfile,
+            self.config,
+        )
+
+        self.assertTrue(protocol.canhandlerequest())
+
+        protocol.handle()
+        self.assertEqual(protocol.httpheaders["host"], "localhost.com")
+
+        response = self.wfile.getvalue().decode(errors="surrogateescape")
+        # Non-UTF8 files should show up in the listing. I don't know how
+        # browsers are supposed to render these, but chrome seems to figure
+        # it out as a ® symbol.
+        self.assertIn('<TD>&nbsp;<A HREF="/%AE.txt"><TT>\udcae.txt', response)
 
     def test_http_handler_icon(self):
         request = "GET /PYGOPHERD-HTTPPROTO-ICONS/text.gif HTTP/1.1"
