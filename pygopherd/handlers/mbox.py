76267b8d1dbd3f4e8fe083ef8465bb2da5e24b17
Michael Lazar <lazar.michael22@gmail.com>
2021-01-16 14:43:16 -0500

    PACKAGE: Remove license stubs from the top of every file.
    These notices are unnecessary to enforce GPL, they're ugly, and
    they're a pain in the ass to keep updated. They do not enforce
    anything that isn't already spelled out in the COPYING file.
    
    My understanding is that GPL does not require that these duplicate
    notices are preserved (only the overriding one in the COPYING file).

--- pygopherd/handlers/mbox.py
+++ pygopherd/handlers/mbox.py
@@ -1,21 +1,3 @@
-# pygopherd -- Gopher-based protocol server in Python
-# module: Present a mbox file as if it were a folder.
-# Copyright (C) 2002, 2005 John Goerzen
-# <jgoerzen@complete.org>
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; version 2 of the License.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 import os.path
 import re
 import stat
--- runtests.py
+++ runtests.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python3
-# Python-based gopher server
-# Module: main test runner
-# COPYRIGHT #
-# Copyright (C) 2021 Michael Lazar
-# Copyright (C) 2002-2019 John Goerzen
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; version 2 of the License.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-# END OF COPYRIGHT #
 import sys
 import tracemalloc
 import unittest
