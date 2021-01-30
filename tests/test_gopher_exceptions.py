de21fa8cde9b3096b3ab1795a718fb1946013eec
Michael Lazar <lazar.michael22@gmail.com>
2021-01-30 16:13:05 -0500

    PACKAGE: Remove more copyright stubs.
    See earlier commit message for my rationale on removing these.

--- tests/test_gopher_exceptions.py
+++ tests/test_gopher_exceptions.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python
 
-# Python-based gopher server
-# Module: test of Gopher Exceptions
-# COPYRIGHT #
-# Copyright (C) 2021 Michael Lazar
-# Copyright (C) 2002 John Goerzen
-
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; version 2 of the License.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-# END OF COPYRIGHT #
-
 import unittest
 from io import BytesIO
 
--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -1,25 +1,4 @@
 #!/usr/bin/python
-
-# Python-based gopher server
-# Module: test of initialization code
-# COPYRIGHT #
-# Copyright (C) 2021 Michael Lazar
-# Copyright (C) 2002 John Goerzen
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; version 2 of the License.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-# END OF COPYRIGHT #
-
 import mimetypes
 import socketserver
 import unittest
